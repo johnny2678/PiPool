2b85a2a03bf9b090ec1a98b3e17a68e93b4f5293
John Hill <johnny2678@gmail.com>
2017-05-08 09:09:16 -0400

    updated for flock

--- pi-st.py
+++ pi-st.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+import os
 import sys
 import glob
 import re
@@ -12,11 +13,7 @@ import subprocess
 import datetime
 import re
 import logging
-#import logging.config
 
-#logging.config.fileConfig('/home/pi/logging.conf', disable_existing_loggers=False)
-#logging.fileConfig(loginpath, defaults={'logfilename': '/home/pi/temp.log'})
-#logging.getLogger("urllib3").setLevel(logging.WARNING)
 
 #================================
 # Logging
@@ -30,6 +27,7 @@ formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 
 fh = RotatingFileHandler('/var/log/PiPool/PiPool.log', maxBytes=10000, backupCount=10)
 fh.setLevel(logging.INFO)
+#fh.setLevel(logging.DEBUG)
 fh.setFormatter(formatter)
 
 sh = logging.StreamHandler(stream=sys.stdout)
@@ -41,6 +39,7 @@ logger.addHandler(fh)
 logger.addHandler(sh)
 
 #=================================
+
 base_dir = '/sys/bus/w1/devices/'
 
 #device name array
@@ -59,7 +58,7 @@ dsid = [ '28-800000262ce2'
 
 #device APP client ID (get from IDE -> App Settings)
 client_id = ['b0fcacd9-d55a-4b87-a64a-80eb61a7a948'
-,            '4c31867c-304b-4aa9-8df1-3fc2f0d99e8f'
+,      	     '4c31867c-304b-4aa9-8df1-3fc2f0d99e8f'
 ,            '0f6a05f7-06fb-452e-bf8d-f3006b5e8991'
 #,            '973bfa64-269d-4ce4-8a86-1fff2499b1fd'
 ]
@@ -73,6 +72,15 @@ ds_token= ['8c98af31-267e-4704-aa10-fe696f34c041'
 
 ### no modifications should be needed below this line
 # ===================================================
+# test node.js pool controller call to see if pump is running
+url = 'http://192.168.5.31:3000/pump'
+r = requests.get(url)
+pumps = json.loads(r.text)
+pump_onoff=pumps[1]['power']
+
+if pump_onoff == 0:
+	logging.warning("Pump is not running. Exiting.")
+	exit();
 
 #device files array
 dsfile = []
@@ -80,18 +88,16 @@ for tmpdsid in dsid:
    tmpfile = (base_dir + tmpdsid + '/w1_slave')
    try:
       with open(tmpfile) as file:
-         logging.info("Found DS18B20 sensor %s" % (tmpdsid))
+       	 logging.info("Found DS18B20 sensor %s" % (tmpdsid))
          pass
    except IOError as e:
       logging.error("Could not find DS18B20 sensor %s. Exiting." % (tmpfile))
       exit();
-
    dsfile.append(base_dir + tmpdsid + '/w1_slave')
 
+niters = 6
+interval = 15
 
-niters = 60
-interval = 30
- 
 def read_temp_raw(cntc):
     logging.debug ("\t\tread_temp_raw(%i): %s" % (cntc,dsfile[cntc]))
     catdata = subprocess.Popen(['cat',dsfile[cntc]], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -99,20 +105,20 @@ def read_temp_raw(cntc):
     out_decode = out.decode('utf-8')
     lines = out_decode.split('\n')
     return lines
- 
+
 def read_temp(cntb):
     lines = read_temp_raw(cntb)
     logging.debug ("\tread_temp(%i): %s" % (cntb,lines))
     while lines[0].strip()[-3:] != 'YES':
-        time.sleep(0.2)
+       	time.sleep(0.2)
         lines = read_temp_raw(cntb)
     equals_pos = lines[1].find('t=')
-    if equals_pos != -1:
-        temp_string = lines[1][equals_pos+2:]
-        temp_c = float(temp_string) / 1000.0
-        temp_f = temp_c * 9.0 / 5.0 + 32.0
-        return temp_c, temp_f
 
+    if equals_pos != -1:
+       	temp_string = lines[1][equals_pos+2:]
+        temp_c = float(temp_string) / 1000.0
+       	temp_f = temp_c * 9.0 / 5.0 + 32.0
+        return temp_c, temp_f
 def main():
    old_temp = []
    temp_url = []
@@ -126,14 +132,14 @@ def main():
        req =  requests.get(get_endpoints)
        if (req.status_code != 200):
           logging.error ("Error: " + str(r.status_code) + "exiting.")
-          exit()
+       	  exit()
        else:
-          logging.info ("Endpoint URL for %s found!..." % (dsname[i]))
+       	  logging.info ("Endpoint URL for %s found!..." % (dsname[i]))
 
        endpoints = json.loads( req.text )
-          
+         
        for endp in endpoints:
-          uri = endp['uri']
+       	  uri = endp['uri']
           temp_url.append( uri + ("/update/"))
              
        i += 1
@@ -141,18 +147,18 @@ def main():
    for counter in range(niters, 0, -1):
        cnta=0
        for tmpds in dsid:
-          (temp_c, temp_f) = read_temp(cnta)
+       	  (temp_c, temp_f) = read_temp(cnta)
           endp_url = temp_url[cnta] + ("%.2f/F" % temp_f)     
-          headers = { 'Authorization' : 'Bearer ' + ds_token[cnta] } 
+       	  headers = { 'Authorization' : 'Bearer ' + ds_token[cnta] } 
           
 #          if ( round(temp_f, 2) != round(old_temp[cnta], 2) ):
           if ( abs(round(temp_f, 2) - round(old_temp[cnta], 2)) > 0.18 and lower_submit_limit <= temp_f <= upper_submit_limit):
              logging.debug ("Endpoint submit URL: %s" % (endp_url))
-             logging.info ("Niter: %s\tdsname: %s\ttemp_f: %s" % (counter, dsname[cnta], temp_f))
+       	     logging.info ("Niter: %s\tdsname: %s\ttemp_f: %s" % (counter, dsname[cnta], temp_f))
 
              r = requests.put(endp_url, headers=headers)
 
-             old_temp[cnta] = temp_f
+       	     old_temp[cnta] = temp_f
 
           cnta += 1
 
